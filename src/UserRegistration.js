5a49a7b60d7f337f172bada87ffc2651ac848518
Michelle Stone <michellebstone@gmail.com>
2023-11-03 23:33:42 -0500

    Attempt to add a log in page

---
+++ src/UserRegistration.js
@@ -0,0 +1,39 @@
+import React, { FC, ReactElement, useState } from "react";
+import { Button, StyleSheet, TextInput } from "react-native";
+import Parse from "parse/react-native";
+
+export const UserRegistration = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        value={username}
+        placeholder={"Username"}
+        onChangeText={(text) => setUsername(text)}
+        autoCapitalize={"none"}
+      />
+      <TextInput
+        style={styles.input}
+        value={password}
+        placeholder={"Password"}
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button title={"Sign Up"} onPress={() => {}} />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    marginBottom: 10,
+    backgroundColor: '#fff',
+  },
+});
+
+
+// Below code is from https://alitalhacoban.medium.com/basic-login-screen-with-react-native-c9f7fdcc8dae
